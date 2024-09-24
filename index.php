6a87eba4df53e760436c432b4eedb010cdf9b121
Callisyaaaaa <160854587+Callisyaaaaa@users.noreply.github.com>
2024-09-24 10:40:53 -0700

    index

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Paket Wisata</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+<body>
+    <div class="container">
+    <h1>
+        <span class="header-top"> LIBRE </span><br>
+        <span class="header-bottom">Travel & Tour</span>
+</h1>
