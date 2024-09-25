c61bc179dad77dbb7dc6026b2b2b670c15920631
pelinlimited <felinnn96@gmail.com>
2024-09-25 16:37:51 +0700

    confirmation2

--- confirmation.php
+++ confirmation.php
@@ -29,3 +29,11 @@ if ($selectedPackage === null) {
     <div class="confirmation-container">
         <h2>Terima kasih, <?php echo $_SESSION['name']; ?>!</h2>
         <p>Pemesanan Anda telah diterima.</p>
+        
+        <div class="data-container">
+            <strong>Detail Pemesanan:</strong>
+            <ul>
+                <li>Nama: <?php echo $_SESSION['name']; ?></li>
+                <li>Email: <?php echo $_SESSION['email']; ?></li>
+            </ul>
+        </div>
