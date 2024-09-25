13048413d4c194cd47440b7613b2f45360a473be
ShellaMarcelia <160854951+ShellaMarcelia@users.noreply.github.com>
2024-09-25 11:06:10 +0700

    booking 1

---
+++ booking.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booking Paket Wisata</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="booking-container">
+        <h2>PEMESANAN PAKET TOUR</h2>
+    </div>
+</body>
+</html>
