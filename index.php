0bea083ff1718d40b03666bcd66bd879bf4cc0d3
Callisyaaaaa <160854587+Callisyaaaaa@users.noreply.github.com>
2024-09-25 05:10:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if ($packages === null) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paket Wisata</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type ="text/css" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 
 </head>
